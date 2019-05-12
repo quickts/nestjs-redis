2a49e4612418674f1e106cfe96982c7e54a99f0d
chengaoyuan <chen.gaoyuan@qq.com>
2019-05-13 00:34:17 +0800

    增加executeCommand executeScript

--- src/redis/redis.client.ts
+++ src/redis/redis.client.ts
@@ -1,5 +1,31 @@
 import { Injectable } from "@nestjs/common";
+import { createHash } from "crypto";
 import * as Redis from "ioredis";
 
 @Injectable()
-export class RedisClient extends Redis {}
+export class RedisClient extends Redis {
+    private shaCache: { [script: string]: string } = {};
+
+    public executeCommand(cmd: string, ...args: any[]) {
+        return (this as any)[cmd](...args);
+    }
+
+    public async executeScript(script: string, numKeys: number, ...args: (string | number)[]) {
+        let sha = this.shaCache[script];
+        if (!sha) {
+            sha = createHash("sha1")
+                .update(script)
+                .digest("hex")
+                .toLocaleLowerCase();
+            this.shaCache[script] = sha;
+        }
+        try {
+            return await this.evalsha(sha, numKeys, ...args);
+        } catch (err) {
+            if (err.toString().indexOf("NOSCRIPT") === -1) {
+                throw err;
+            }
+            return await this.eval(script, numKeys, ...args);
+        }
+    }
+}
