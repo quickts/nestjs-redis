0e1a9879bc97ee3c70a323b8e379dba2fc4ae716
chengaoyuan <chen.gaoyuan@qq.com>
2019-05-13 14:12:23 +0800

    增加自定义token

--- src/redis/redis.provider.ts
+++ src/redis/redis.provider.ts
@@ -2,10 +2,10 @@ import { Provider } from "@nestjs/common";
 import { RedisOptions } from "./redis.interface";
 import { RedisClient } from "./redis.client";
 
-export function createProvider(redisOptions: RedisOptions): Provider<RedisClient> {
+export function createProvider(redisOptions: RedisOptions, token?: any): Provider<RedisClient> {
     const client = new RedisClient(redisOptions);
     return {
-        provide: RedisClient,
+        provide: token === undefined ? RedisClient : token,
         useValue: client
     };
 }
