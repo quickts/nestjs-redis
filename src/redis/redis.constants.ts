430b865eaef438b5d4d2544599ad08a6be86b29a
chengaoyuan <chen.gaoyuan@qq.com>
2019-10-26 16:13:54 +0800

    增加command初始化回调

--- src/redis/redis.constants.ts
+++ src/redis/redis.constants.ts
@@ -1,2 +1,3 @@
 export const REDIS_OPTION = Symbol("REDIS_OPTION");
+export const COMMAND_ADDER = Symbol("COMMAND_ADDER");
 export const REDIS_CLIENT_METADATA = "quickts:redis:client";
--- src/redis/redis.interface.ts
+++ src/redis/redis.interface.ts
@@ -3,3 +3,7 @@ import { Redis } from "ioredis";
 export interface OnRedisClientInit {
     onRedisClientInit(redis: Redis): any;
 }
+
+export interface CommandAdder {
+    (redis: Redis): any;
+}
